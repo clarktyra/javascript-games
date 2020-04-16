917174e45678dfce6eba7e92b3a0b3d986f2527b
Clark Tyra <clark.tyra@gmail.com>
2020-04-15 20:55:45 -0700

    added the rock paper scissor columns, css file, and javascript file

---
+++ js/rockpapaerscissors.js
@@ -0,0 +1,18 @@
+//define and assign variables for computerChoice and playerChoice
+function playGame(){
+    var choices = ["rock", "paper", "scissors"];
+    var computerChoice = choices[Math.floor(Math.random() * 3)]
+    
+    console.log(computerChoice)
+
+    // var userChoice = prompt("Type either rock, paper, or scissors")
+    console.log(userChoice)
+    // alert("The computer picked " + computerChoice)
+    
+
+
+    }
+
+//
+    
+    playGame();
