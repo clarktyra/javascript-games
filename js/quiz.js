5994b4faefff921ab4351120809b274b6fa2750d
Clark Tyra <clark.tyra@gmail.com>
2020-04-16 22:04:50 -0700

    added questions array

--- js/quiz.js
+++ js/quiz.js
@@ -1,1 +1,17 @@
-var test = test;
+var questions = [
+    {
+        question: "what is the capital of Maryland?",
+        options: ["Baltimore", "Annapolis", "Potomac", "Cumberland"],
+        answer: "Annapolis"
+    },
+    {
+        question: "what is the capital of Virginia?",
+        options: ["McClean", "Jamestown", "Richmond", "Williamsburg"],
+        answer: "Richmond"
+    },
+    {
+        question: "what is the capital of Pennsylvania?",
+        options: ["Philidelphia", "Pittsburg", "Harrisburg", "Lancaster"],
+        answer: "Philidelphia"
+    }
+]
