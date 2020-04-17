d6e49d994611db8c8a572e7f867ae536ebf860cc
Clark Tyra <clark.tyra@gmail.com>
2020-04-16 22:55:42 -0700

    got functions startTimer, clockTick, and quizOver working

--- js/quiz.js
+++ js/quiz.js
@@ -1,17 +1,48 @@
 var questions = [
-    {
-        question: "what is the capital of Maryland?",
-        options: ["Baltimore", "Annapolis", "Potomac", "Cumberland"],
-        answer: "Annapolis"
-    },
-    {
-        question: "what is the capital of Virginia?",
-        options: ["McClean", "Jamestown", "Richmond", "Williamsburg"],
-        answer: "Richmond"
-    },
-    {
-        question: "what is the capital of Pennsylvania?",
-        options: ["Philidelphia", "Pittsburg", "Harrisburg", "Lancaster"],
-        answer: "Philidelphia"
+  {
+    question: "what is the capital of Maryland?",
+    options: ["Baltimore", "Annapolis", "Potomac", "Cumberland"],
+    answer: "Annapolis"
+  },
+  {
+    question: "what is the capital of Virginia?",
+    options: ["McClean", "Jamestown", "Richmond", "Williamsburg"],
+    answer: "Richmond"
+  },
+  {
+    question: "what is the capital of Pennsylvania?",
+    options: ["Philidelphia", "Pittsburg", "Harrisburg", "Lancaster"],
+    answer: "Philidelphia"
+  }
+];
+
+var startButton = document.getElementById("start");
+var time       = document.getElementById("time");
+var questions   = document.getElementById("questions");
+var choices     = document.getElementById("choices");
+var submit      = document.getElementById("submit");
+var feedback    = document.getElementById("feedback");
+var initials    = document.getElementById("initials");
+
+var seconds = 5;
+
+function startTimer(){
+    event.preventDefault();
+    timerId = setInterval(clockTick, 1000);
+}
+
+function clockTick() {
+    seconds--;
+    time.textContent = seconds;
+    if (seconds <= 0) {
+      quizOver();
     }
-]
+  }
+
+function quizOver(){
+    clearInterval(timerId)
+    console.log("Quiz over")
+}
+
+startButton.addEventListener("click", startTimer);
+
