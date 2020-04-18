f3c04fe4a6e53423069ba8ff800db3510d6cd804
Clark Tyra <clark.tyra@gmail.com>
2020-04-17 22:47:30 -0700

    made the getScore function, got a Play Again button working

--- js/quiz.js
+++ js/quiz.js
@@ -13,22 +13,65 @@ var questions = [
     question: "what is the capital of Pennsylvania?",
     options: ["Philidelphia", "Pittsburg", "Harrisburg", "Lancaster"],
     answer: "Philidelphia"
-  }
+   }
+  //,
+  // {
+  //   question: "what is the capital of New Jersey?",
+  //   options: ["Baltimore", "Annapolis", "Potomac", "Cumberland"],
+  //   answer: "Annapolis"
+  // },
+  // {
+  //   question: "what is the capital of New York?",
+  //   options: ["McClean", "Jamestown", "Richmond", "Williamsburg"],
+  //   answer: "Richmond"
+  // },
+  // {
+  //   question: "what is the capital of Connecticut?",
+  //   options: ["Philidelphia", "Pittsburg", "Harrisburg", "Lancaster"],
+  //   answer: "Philidelphia"
+  // }, {
+  //   question: "what is the capital of Rhode Island?",
+  //   options: ["Baltimore", "Annapolis", "Potomac", "Cumberland"],
+  //   answer: "Annapolis"
+  // },
+  // {
+  //   question: "what is the capital of Main?",
+  //   options: ["McClean", "Jamestown", "Richmond", "Williamsburg"],
+  //   answer: "Richmond"
+  // },
+  // {
+  //   question: "what is the capital of New Hampshire?",
+  //   options: ["Philidelphia", "Pittsburg", "Harrisburg", "Lancaster"],
+  //   answer: "Philidelphia"
+  // },
+  // {
+  //   question: "what is the capital of Vermont?",
+  //   options: ["Philidelphia", "Pittsburg", "Harrisburg", "Lancaster"],
+  //   answer: "Philidelphia"
+  // }
 ];
 
 var startButton = document.getElementById("start");
 var time        = document.getElementById("time");
-var questionArea   = document.getElementById("questions");
+var questionArea = document.getElementById("questions");
 var choices     = document.getElementById("choices");
 var submit      = document.getElementById("submit");
 var feedback    = document.getElementById("feedback");
 var initials    = document.getElementById("initials");
+var endScreen = document.getElementById("end-screen");
 
 var seconds = 5;
 var score = 0;
+var userChoices = [];
+var questionNumber = 0;
 
 function startTimer(){
     event.preventDefault();
+    seconds = 5;
+    score = 0;
+    questionNumber = 0
+    userChoices = [];
+    endScreen.setAttribute("class", "hide");
     timerId = setInterval(clockTick, 1000);
     var startScreen = document.getElementById("start-screen");
     startScreen.setAttribute("class", "hide")
@@ -48,12 +91,12 @@ function quizOver(){
     clearInterval(timerId)
     console.log("Quiz over")
     questionArea.setAttribute("class", "hide")
-    var endScreen = document.getElementById("end-screen");
     endScreen.removeAttribute("class", "hide");
     var finalScore = document.getElementById("final-score");
+    getScore();
     finalScore.textContent = score;
 }
-var questionNumber = 0;
+
 function showQuestion() {
   var questionTitle = document.getElementById("question-title");
   questionTitle.textContent = questions[questionNumber].question;
@@ -62,17 +105,33 @@ function showQuestion() {
     for (let i = 0; i < questions[questionNumber].options.length; i++) {
       var choicesButton = document.createElement("button");
       choicesButton.textContent = questions[questionNumber].options[i];
+      choicesButton.setAttribute("value", questions[questionNumber].options[i])
       choicesButton.onclick = questionSubmit;
       choices.appendChild(choicesButton);
     }
-  
 }
 
 function questionSubmit(){
   questionNumber++;
-  showQuestion()
+  userChoices.push(this.value)
+  if(questionNumber < questions.length){
+    showQuestion()
+  } else {
+    quizOver()
+  }
+}
+
+function getScore(){
+  var answers = questions.map(piece => piece.answer);
+  console.log(answers)
+  console.log(userChoices)
+  for(let a = 0; a < answers.length; a++){
+    if(answers[a]===userChoices[a]){
+      score++
+    }
+  }
+
 }
 
 startButton.addEventListener("click", startTimer);
 
-console.log(questions.length)
