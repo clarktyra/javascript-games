24e297c082a2625a5c42ffb95293100134b76f0e
Clark Tyra <clark.tyra@gmail.com>
2020-04-18 09:18:51 -0700

    made media query for the images for rock,papaer,scissors

--- js/rockpapaerscissors.js
+++ js/rockpapaerscissors.js
@@ -13,11 +13,11 @@ function printComChoice(){
     var choices = ["rock", "paper", "scissors"];
     computerChoice = choices[Math.floor(Math.random() * 3)]
     if(computerChoice === "rock"){
-     document.getElementById("choiceArea").innerHTML = `<img style="width: 200px"  src="images/5a35c7b6db90f5.0034759615134739748994.png"/>`
+     document.getElementById("choiceArea").innerHTML = `<img  src="images/5a35c7b6db90f5.0034759615134739748994.png"/>`
     } else if(computerChoice === "paper"){
-        document.getElementById("choiceArea").innerHTML = `<img style="width: 200px"  src="images/kisspng-post-it-note-paper-clip-art-post-it-note-png-5ab44be02d5f60.7688322515217653441859.png"/>`
+        document.getElementById("choiceArea").innerHTML = `<img  src="images/kisspng-post-it-note-paper-clip-art-post-it-note-png-5ab44be02d5f60.7688322515217653441859.png"/>`
     } else if(computerChoice === "scissors"){
-        document.getElementById("choiceArea").innerHTML = `<img style="width: 200px"  src="images/kisspng-scissors-clip-art-scissors-5a80f8a6d8be97.1303518815184017028878.png"/>`
+        document.getElementById("choiceArea").innerHTML = `<img  src="images/kisspng-scissors-clip-art-scissors-5a80f8a6d8be97.1303518815184017028878.png"/>`
     }
     winner();
 }
